0117ed3511b7219e58ab1d06b72f7e7af5d25274
vimitali368 <vimitali368@gmail.com>
2022-02-09 15:28:16 +0300

    j1 subtask4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function() {
+        Route::get('/', 'IndexController');
+    });
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
