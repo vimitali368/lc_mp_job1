b5fa32c0e55a4305617250780a8541889a546093
vimitali368 <vimitali368@gmail.com>
2022-02-13 17:16:33 +0300

    5_j1_subtask5-8 CRUD breadcrumb

--- resources/views/admin/culture/index.blade.php
+++ resources/views/admin/culture/index.blade.php
@@ -11,8 +11,8 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin') }}">Админка</a></li>
+                            <li class="breadcrumb-item active">Группа культур / Список</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
