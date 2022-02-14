46bdf0058fd36fd5abfd044fc8bbd53f35161ab8
vimitali368 <vimitali368@gmail.com>
2022-02-14 22:15:37 +0300

    17_j1_subtask9 SoftDeletes

--- app/Models/Culture.php
+++ app/Models/Culture.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Culture extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'cultures';
     protected $guarded = false;
