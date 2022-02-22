da29fe4a9a493c0df62e89e8c2ca528769f4667e
vimitali368 <vimitali368@gmail.com>
2022-02-22 09:15:23 +0300

    27_j1_subtask13 fake Login ivanov, middleware auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -407,15 +407,14 @@
             @auth
                 <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
             @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline"Вход</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                @endif
+{{--                @if (Route::has('register'))--}}
+{{--                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>--}}
+{{--                @endif--}}
             @endauth
         </div>
     @endif
-
     <div>
         Супер сайт Иванова
     </div>
