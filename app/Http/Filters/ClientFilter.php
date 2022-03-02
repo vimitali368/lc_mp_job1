9847cb3360e64663edd0abbd80dfeb2a20b64be5
vimitali368 <vimitali368@gmail.com>
2022-03-02 14:29:46 +0300

    27_j1_13 refactoring Client filter - from & to

--- app/Http/Filters/ClientFilter.php
+++ app/Http/Filters/ClientFilter.php
@@ -8,7 +8,11 @@ class ClientFilter extends AbstractFilter
 {
     public const NAME = 'name';
     public const CONTRACT_DATE = 'contract_date';
+    public const CONTRACT_DATE_FROM = 'contract_date_from';
+    public const CONTRACT_DATE_TO = 'contract_date_to';
     public const DELIVERY_COST = 'delivery_cost';
+    public const DELIVERY_COST_FROM = 'delivery_cost_from';
+    public const DELIVERY_COST_TO = 'delivery_cost_to';
     public const REGION = 'regions';
     public const SORT = 'sort';
 
@@ -18,7 +22,11 @@ class ClientFilter extends AbstractFilter
         return [
             self::NAME => [$this, 'name'],
             self::CONTRACT_DATE => [$this, 'contractDate'],
+            self::CONTRACT_DATE_FROM => [$this, 'contractDateFrom'],
+            self::CONTRACT_DATE_TO => [$this, 'contractDateTo'],
             self::DELIVERY_COST => [$this, 'deliveryСost'],
+            self::DELIVERY_COST_FROM => [$this, 'deliveryСostFrom'],
+            self::DELIVERY_COST_TO => [$this, 'deliveryСostTo'],
             self::REGION => [$this, 'regions'],
             self::SORT => [$this, 'sort'],
         ];
@@ -32,16 +40,43 @@ class ClientFilter extends AbstractFilter
 
     public function contractDate(Builder $builder, $array)
     {
-        $contractDateFrom = $array[0];
-        $contractDateTo = $array[1];
+        $valueFrom = $array[0];
+        $valueTo = $array[1];
 //        dd($builder);
-        $builder->whereBetween('contract_date', [$contractDateFrom, $contractDateTo]);
+        $builder->whereBetween('contract_date', [$valueFrom, $valueTo]);
     }
 
-    public function deliveryСost(Builder $builder, $value)
+    public function contractDateFrom(Builder $builder, $value)
     {
-//        dd($value);
-        $builder->whereBetween('delivery_cost', $value);
+        $valueFrom = $value;
+//        dd($valueFrom);
+//        dd($builder);
+        $builder->where('contract_date', '>=', $value);
+    }
+
+    public function contractDateTo(Builder $builder, $value)
+    {
+        $valueTo = $value;
+//        dd($valueTo);
+//        dd($builder);
+        $builder->where('contract_date', '<=', $value);
+    }
+
+    public function deliveryСost(Builder $builder, $array)
+    {
+        $valueFrom = $array[0];
+        $valueTo = $array[1];
+        $builder->whereBetween('delivery_cost', [$valueFrom, $valueTo]);
+    }
+
+    public function deliveryСostFrom(Builder $builder, $value)
+    {
+        $builder->where('delivery_cost', '>=', $value);
+    }
+
+    public function deliveryСostTo(Builder $builder, $value)
+    {
+        $builder->where('delivery_cost', '<=', $value);
     }
 
     public function regions(Builder $builder, $arrayOfIds)
